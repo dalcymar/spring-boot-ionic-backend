7aa79a792a2c46dd87f93569d5bb67b0f86a4d45
Dalcimar Luiz <dalcymar.oliveira@gmail.com>
2022-04-19 10:21:08 -0300

    Criado a classe Produto e associaçao de muitos para muitos

--- src/main/java/com/dalcy/CursomcApplication.java
+++ src/main/java/com/dalcy/CursomcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.dalcy.domain.Categoria;
+import com.dalcy.domain.Produto;
 import com.dalcy.repositories.CategoriaRepository;
+import com.dalcy.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -16,20 +18,52 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		//Instanciado a categoria 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório Tecnologico");
 		
-		//categoriaRepository.save((S) Arrays.asList(cat1, cat2));
+		//Instanciado o produto
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse",  80.00);
+		
+		
+		// essa forma é mais rapida e gasta menos espaço
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		// Posso usar essa forma porém gastarei mas espaço.
+		/*
+		 	cat1.getProdutos().add(p1);
+			cat1.getProdutos().add(p2);
+			cat1.getProdutos().add(p3);
+		
+			cat2.getProdutos().add(p2);
+		*/
+		
+		
+		//produtos
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.save(cat1);
 		categoriaRepository.save(cat2);
 		
+		// Salvar os produtos
+		//produtoRepository.save(Arrays.asList(p1, p2, p3));
+		produtoRepository.save(p1);
+		produtoRepository.save(p2);
+		produtoRepository.save(p3);
 	}
 
 	
