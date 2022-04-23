fdb74606e2b5285460828a1f504971c60a346a24
Dalcimar Luiz <dalcymar.oliveira@gmail.com>
2022-04-23 15:56:04 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/dalcy/services/CategoriaService.java
+++ src/main/java/com/dalcy/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.dalcy.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.dalcy.domain.Categoria;
 import com.dalcy.repositories.CategoriaRepository;
+import com.dalcy.services.exceptions.DataIntegrityException;
 import com.dalcy.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -38,5 +40,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
+	
 	}
 }
